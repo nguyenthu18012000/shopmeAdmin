5d1dbd57535f24f3d635c99452db6f8bf06c5462
nguyenthu <nguyenthu180100@gmail.com>
2024-10-16 11:33:12 +0700

    add fontawsome icon

--- src/app/pages/user/list-user/list-user.component.ts
+++ src/app/pages/user/list-user/list-user.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { MainLayoutComponent } from '../../../components/main-layout/main-layout.component';
-import { UserService } from '../../../../services/user.service';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+
+import {
+  faPortrait,
+  faEdit,
+  faTrash,
+  faCheckCircle,
+} from '@fortawesome/free-solid-svg-icons';
+import { UserService } from '../../../../services/user.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MainLayoutComponent } from '../../../components/main-layout/main-layout.component';
 
 @Component({
   selector: 'app-list-user',
   standalone: true,
-  imports: [MainLayoutComponent, CommonModule, RouterLink, RouterLinkActive],
+  imports: [
+    MainLayoutComponent,
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    FontAwesomeModule,
+  ],
   templateUrl: './list-user.component.html',
   styleUrl: './list-user.component.scss',
 })
@@ -15,6 +29,10 @@ export class ListUserComponent {
   constructor(private userService: UserService) {}
 
   listUser: any[] = [];
+  faPortrait = faPortrait;
+  faEdit = faEdit;
+  faTrash = faTrash;
+  faCheckCircle = faCheckCircle;
 
   ngOnInit() {
     this.userService
