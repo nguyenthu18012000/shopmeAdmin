1510add710d0ceb50c00b4db0727eb941a0a4cf0
nguyenthu <nguyenthu180100@gmail.com>
2024-10-21 13:29:35 +0700

    config base url

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -6,14 +6,18 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
+  private baseUrl = environment.baseUrl;
+
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     request = request.clone({
+      url: `${this.baseUrl}${request.url}`,
       setHeaders: {
         Authorization: `Bearer test_token`,
       },
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,37 +8,32 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   getListUser() {
-    return this.http.get('http://localhost:8080/ShopmeAdmin/users', {});
+    return this.http.get('ShopmeAdmin/users', {});
   }
 
   getListUserRole() {
-    return this.http.get('http://localhost:8080/ShopmeAdmin/users/roles', {});
+    return this.http.get('ShopmeAdmin/users/roles', {});
   }
 
   createUser(newUser: any) {
-    return this.http.post(
-      'http://localhost:8080/ShopmeAdmin/users/new',
-      newUser,
-      { responseType: 'text' }
-    );
+    return this.http.post('ShopmeAdmin/users/new', newUser, {
+      responseType: 'text',
+    });
   }
 
   getUserById(id: number) {
-    return this.http.get(`http://localhost:8080/ShopmeAdmin/users/${id}`);
+    return this.http.get(`ShopmeAdmin/users/${id}`);
   }
 
   editUser(user: any) {
-    return this.http.put('http://localhost:8080/ShopmeAdmin/users/edit', user, {
+    return this.http.put('ShopmeAdmin/users/edit', user, {
       responseType: 'text',
     });
   }
 
   deleteUser(id: number) {
-    return this.http.delete(
-      `http://localhost:8080/ShopmeAdmin/users/delete/${id}`,
-      {
-        responseType: 'text',
-      }
-    );
+    return this.http.delete(`ShopmeAdmin/users/delete/${id}`, {
+      responseType: 'text',
+    });
   }
 }
