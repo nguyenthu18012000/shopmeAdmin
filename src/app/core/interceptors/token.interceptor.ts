d5416a2e2c419ca0daa3eb3f4abf60decde58ce7
nguyenthu <nguyenthu180100@gmail.com>
2024-10-22 10:37:19 +0700

    test inteceptor

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -3,14 +3,20 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
+  HttpResponse,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+
 import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
   private baseUrl = environment.baseUrl;
+
+  private modifyBody(body: any) {
+    // console.log(body);
+  }
 
   intercept(
     request: HttpRequest<any>,
@@ -23,6 +29,14 @@ export class TokenInterceptor implements HttpInterceptor {
       },
     });
 
-    return next.handle(request);
+    return next.handle(request).pipe(
+      map((event: HttpEvent<any>) => {
+        console.log(event);
+        if (event instanceof HttpResponse) {
+          event = event.clone({ body: this.modifyBody(event.body) });
+        }
+        return event;
+      })
+    );
   }
 }
