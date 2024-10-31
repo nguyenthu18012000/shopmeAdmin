7273e6211bc082087a3bef7984e2d694564e4984
nguyenthu <nguyenthu180100@gmail.com>
2024-10-31 15:15:56 +0700

    add pagination component

--- src/app/shared/components/main-layout/main-layout.component.ts
+++ src/app/shared/components/main-layout/main-layout.component.ts
@@ -6,6 +6,8 @@ import {
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
+import { PaginationComponent } from '../pagination/pagination.component';
+
 @Component({
   selector: 'app-main-layout',
   standalone: true,
@@ -15,8 +17,16 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
     NgbModule,
     RouterLink,
     RouterLinkActive,
+    PaginationComponent,
   ],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent {
+  current = 7;
+  total = 20;
+  onGoTo(page: any) {
+    this.current = page;
+    console.log(page);
+  }
+}
